4a1072d05137b2bc60a20d7e52871c5c36ea9fa1
ssy940215 <59674489+ssy940215@users.noreply.github.com>
2020-02-03 16:41:37 +0800

    Create HisApplicationTests.java
    创建实体类

---
+++ src/test/java/com/neusoft/his/HisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neusoft.his;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
